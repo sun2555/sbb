317516e997091175a2f3d642045e04b56b315ae6
soyul <sun25555@gmail.com>
2024-09-27 10:52:17 +0900

    프로젝트 초기화

---
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+//    @ResponseBody
+    public String hello() {
+        return "Hello";
+    }
+    
+    @GetMapping("/event")
+    public String event() {
+    	String filename = "myevent";
+        return "myevent";
+    }
+    
+}
